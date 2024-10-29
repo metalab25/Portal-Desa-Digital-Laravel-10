178d22e33af1a6d02bd9180f98f778e1e5ac119f
METALABPC\metal <metalabmetadata@gmail.com>
2024-10-29 09:46:10 +0800

    Penambahan fungsi edit dan perbagian label

--- resources/views/dashboard/pamong/edit.blade.php
+++ resources/views/dashboard/pamong/edit.blade.php
@@ -3,8 +3,9 @@
 @section('content')
     <div class="app-content">
         <div class="container-fluid">
-            <form action="{{ route('pemerintah.store') }}" method="post" enctype="multipart/form-data">
+            <form action="{{ route('pemerintah.update', $pamong->id) }}" method="post" enctype="multipart/form-data">
                 @csrf
+                @method('PUT')
                 <div class="row">
                     <div class="col-md-4">
                         <div class="card rounded-3 mb-3">
@@ -15,8 +16,14 @@
                             </div>
                             <div class="card-body">
                                 <div class="d-flex justify-content-center">
-                                    <img src="{{ asset('assets/dashboard/img/no-picture.webp') }}"
-                                        class="foto-preview rounded-4 img-fluid d-block mx-auto mb-2">
+                                    <input type="hidden" name="oldFoto" value="{{ $pamong->foto }}">
+                                    @if ($pamong->foto)
+                                        <img src="{{ asset('storage/' . $pamong->foto) }}"
+                                            class="foto-preview rounded-4 img-fluid d-block mx-auto mb-3">
+                                    @else
+                                        <img src="{{ asset('/assets/dashboard/img/no-picture.webp') }}"
+                                            class="foto-preview rounded-4 img-fluid d-block mx-auto mb-3">
+                                    @endif
                                 </div>
                                 <div class="mb-0">
                                     <p class="text-center text-xs text-danger mb-2">Maksimal 2MB dengan ukuran 400px x 600px
@@ -46,7 +53,7 @@
                                         <select id="penduduk_id" name="penduduk_id" class="form-control form-select">
                                             <option value="">-- Pilih Penduduk -- </option>
                                             @foreach ($penduduk as $item)
-                                                @if (old('penduduk_id') == $item->id)
+                                                @if (old('penduduk_id', $pamong->penduduk_id) == $item->id)
                                                     <option value="{{ $item->id }}" selected>
                                                         {{ $item->nama . ' - NIK : ' . $item->nik }}
                                                     </option>
@@ -73,7 +80,7 @@
                                     <div class="col-md-10">
                                         <input type="text" name="jabatan" id="jabatan"
                                             class="form-control  @error('jabatan') is-invalid @enderror"
-                                            value="{{ old('jabatan') }}" placeholder="Tuliskan Jabatan">
+                                            value="{{ old('jabatan', $pamong->jabatan) }}" placeholder="Tuliskan Jabatan">
                                         @error('jabatan')
                                             <div class="invalid-feedback">
                                                 {{ 'Jabatan harus diisi' }}
@@ -85,28 +92,29 @@
                                     <label for="nip" class="form-label col-md-2 pt-2">NIP/NIAP</label>
                                     <div class="col-md-10">
                                         <input type="text" name="nip" id="nip" class="form-control"
-                                            value="{{ old('nip') }}" placeholder="Tuliskan NIP/NIAP">
+                                            value="{{ old('nip', $pamong->nip) }}" placeholder="Tuliskan NIP/NIAP">
                                     </div>
                                 </div>
                                 <div class="form-group row mb-3">
                                     <label for="no_sk" class="form-label col-md-2 pt-2">Nomor SK</label>
                                     <div class="col-md-10">
                                         <input type="text" name="no_sk" id="no_sk" class="form-control"
-                                            value="{{ old('no_sk') }}" placeholder="Tuliskan Nomor SK Pengangkatan">
+                                            value="{{ old('no_sk', $pamong->no_sk) }}"
+                                            placeholder="Tuliskan Nomor SK Pengangkatan">
                                     </div>
                                 </div>
                                 <div class="form-group row mb-3">
-                                    <label for="tgl_sk" class="form-label col-md-2 pt-2">Tanggall SK</label>
+                                    <label for="tgl_sk" class="form-label col-md-2 pt-2">Tanggal SK</label>
                                     <div class="col-md-10">
                                         <input type="date" name="tgl_sk" id="tgl_sk" class="form-control"
-                                            value="{{ old('tgl_sk') }}">
+                                            value="{{ old('tgl_sk', $pamong->tgl_sk) }}">
                                     </div>
                                 </div>
                                 <div class="form-group row mb-3">
                                     <label for="masa_jabatan" class="form-label col-md-2 pt-2">Masa Jabatan</label>
                                     <div class="col-md-10">
                                         <input type="text" name="masa_jabatan" id="masa_jabatan" class="form-control"
-                                            value="{{ old('masa_jabatan') }}"
+                                            value="{{ old(', $pamong->no_sk', $pamong->masa_jabatan) }}"
                                             placeholder="Tuliskan masa jabatan sejak tahun sampai dengan tahun">
                                     </div>
                                 </div>
@@ -114,7 +122,7 @@
                                     <label for="nama" class="form-label col-md-2 pt-2">Nama Lengkap</label>
                                     <div class="col-md-6">
                                         <input type="text" name="nama" id="nama" class="form-control"
-                                            value="{{ old('nama') }}" placeholder="Nama Lengkap Pejabat">
+                                            value="{{ old('nama', $pamong->nama) }}" placeholder="Nama Lengkap Pejabat">
                                     </div>
                                     <div class="col-md-4">
                                         <div class="text-sm text-danger pt-2">Diisi apabila bukan penduduk
@@ -125,7 +133,7 @@
                                     <label for="nik" class="form-label col-md-2 pt-2">NIK Pejabat</label>
                                     <div class="col-md-6">
                                         <input type="text" name="nik" id="nik" class="form-control"
-                                            value="{{ old('nik') }}" placeholder="NIK Pejabat">
+                                            value="{{ old('nik', $pamong->nik) }}" placeholder="NIK Pejabat">
                                     </div>
                                     <div class="col-md-4">
                                         <div class="text-sm text-danger pt-2">Diisi apabila bukan penduduk
@@ -139,7 +147,7 @@
                                             class="form-control form-select">
                                             <option value="">-- Pilih Pendidikan -- </option>
                                             @foreach ($pendidikan_kk as $item)
-                                                @if (old('pendidikan_kk_id') == $item->id)
+                                                @if (old('pendidikan_kk_id', $pamong->pendidikan_kk_id) == $item->id)
                                                     <option value="{{ $item->id }}" selected>
                                                         {{ $item->nama }}
                                                     </option>
@@ -160,7 +168,8 @@
                                     <label for="tempat_lahir" class="form-label col-md-2 pt-2">Tempat Lahir</label>
                                     <div class="col-md-6">
                                         <input type="text" name="tempat_lahir" id="tempat_lahir" class="form-control"
-                                            value="{{ old('tempat_lahir') }}" placeholder="Tuliskan Tempat Lahir">
+                                            value="{{ old('tempat_lahir', $pamong->tempat_lahir) }}"
+                                            placeholder="Tuliskan Tempat Lahir">
                                     </div>
                                     <div class="col-md-4">
                                         <div class="text-sm text-danger pt-2">Diisi apabila bukan penduduk
@@ -171,7 +180,8 @@
                                     <label for="tanggal_lahir" class="form-label col-md-2 pt-2">Tanggal Lahir</label>
                                     <div class="col-md-6">
                                         <input type="date" name="tanggal_lahir" id="tanggal_lahir"
-                                            class="form-control" value="{{ old('tanggal_lahir') }}">
+                                            class="form-control"
+                                            value="{{ old('tanggal_lahir', $pamong->tanggal_lahir) }}">
                                     </div>
                                     <div class="col-md-4">
                                         <div class="text-sm text-danger pt-2">Diisi apabila bukan penduduk
