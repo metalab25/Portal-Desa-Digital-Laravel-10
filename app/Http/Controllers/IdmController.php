fb4768f5c7d003cd34d4efaa65fbcef6ce492e79
METALABPC\metal <metalabmetadata@gmail.com>
2024-11-10 00:47:55 +0800

    Menambahkan select tahun untuk fetching data

--- app/Http/Controllers/IdmController.php
+++ app/Http/Controllers/IdmController.php
@@ -2,24 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use App\Models\Idm;
 use App\Models\Config;
 use Illuminate\Http\Request;
-use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Http;
 use RealRashid\SweetAlert\Facades\Alert;
 
 class IdmController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $data = Idm::latest()->first();
-        $data = json_decode($data['data'], true)['mapData']['ROW'];
+        $currentYear = Carbon::now()->year;
+        $years = range($currentYear, $currentYear - 3);
+
+        $selectedYear = $request->input('tahun') ?? $currentYear;
+
+        $dataRecord = Idm::where('tahun', $selectedYear)->latest()->first();
+
+        if (!$dataRecord) {
+            return $this->fetchAndStoreData($selectedYear);
+        }
+
+        $data = json_decode($dataRecord->data, true)['mapData']['ROW'] ?? [];
         $sorted_keys = ["NO", "INDIKATOR", "SKOR", "KETERANGAN", "KEGIATAN", "NILAI", "PUSAT", "PROV", "KAB", "DESA", "CSR", "LAINNYA"];
         $sorted = [];
+
         foreach ($data as $index => $row) {
             foreach ($sorted_keys as $k) {
-                $sorted[$index][$k] = $row[$k];
+                $sorted[$index][$k] = $row[$k] ?? null;
             }
         }
 
@@ -33,55 +44,41 @@ class IdmController extends Controller
             }
         }
 
-        // Hanya ambil 4 data teratas
         $chart_idm = array_slice($data_idm, 0, 3);
 
         return view('dashboard.idm.index', [
-            'page'      => 'Indeks Desa Membangun',
-            'data'      => $data,
-            'data'      => $sorted,
+            'page' => 'Indeks Desa Membangun',
+            'years' => $years,
+            'selectedYear' => $selectedYear,
+            'data' => $sorted,
             'idmSkor'   => $data_idm,
             'idmChart'  => $chart_idm
         ]);
     }
 
-    public function fetchAndStoreData()
+    public function fetchAndStoreData($tahun)
     {
         $desa = Config::first();
-        $kode = $desa->provinsi_id . '' . $desa->kode_kabupaten . '' . $desa->kode_kecamatan . '' . $desa->kode_desa;
-        $year = date('Y');
-        $url  = 'http://idm.kemendesa.go.id/open/api/desa/rumusan/' . $kode . '/' . $year;
-
-        $response = Http::get($url);
-
-        $data = json_decode($response);
-
-        // dd($data);
+        $kode = $desa->provinsi_id . $desa->kode_kabupaten . $desa->kode_kecamatan . $desa->kode_desa;
+        $url  = 'http://idm.kemendesa.go.id/open/api/desa/rumusan/' . $kode . '/' . $tahun;
 
         try {
-            // Request ke API
             $response = Http::get($url);
 
-            // Periksa apakah request berhasil
             if ($response->successful()) {
-                $data = json_decode($response); // Mengambil data dalam bentuk array
+                $data = json_decode($response, true);
+                $jsonToString = json_encode($data);
 
-                $jsonToString = json_encode($data, true);
-
-                // Cek apakah data sudah ada di database untuk tahun ini
-                $existingData = Idm::where('kode_desa', $kode)->where('tahun', $year)->first();
+                $existingData = Idm::where('kode_desa', $kode)->where('tahun', $tahun)->first();
 
                 if ($existingData) {
-                    // Update data jika sudah ada
                     $existingData->data = $jsonToString;
                     $existingData->save();
                 } else {
-                    // Buat entri baru jika belum ada
-
                     Idm::create([
                         'kode_desa' => $kode,
-                        'tahun' => $year,
-                        'data' => $jsonToString,
+                        'tahun'     => $tahun,
+                        'data'      => $jsonToString,
                     ]);
                 }
                 Alert::success('Success', 'Data berhasil disimpan');
